7e131adc9434908aece35ea4568460c323e93037
Judex66 <sermac2000@mail.ru>
2023-02-28 11:43:43 +0300

    add new backgrounds

--- momentum/back.js
+++ momentum/back.js
@@ -1,16 +1,16 @@
-document.body.style.background = "url('assets/img/bg7.jpg')";
+document.body.style.background = "url('assets/img/bg.jpg')";
 var i = 0;
 var k= 0;
 
 function next(){
-var img = ["url('assets/img/bg8.jpg')", "url('assets/img/bg6.jpg')","url('assets/img/bg7.jpg')","url('assets/img/bg.jpg')","url('assets/img/bg8.jpg')","url('assets/img/bg2.jpg')"];
+var img = ["url('assets/img/bg.jpg')", "url('assets/img/bg1.jpg')","url('assets/img/bg2.jpg')"];
     document.body.style.background = img[i];
     i++;
     if( i >= img.length) {
         i = 0;
     }
 }
-var img2 = ["url('assets/img/bg8.jpg')", "url('assets/img/bg.jpg')","url('assets/img/bg6.jpg')","url('assets/img/bg8.jpg')","url('assets/img/bg2.jpg')","url('assets/img/bg7.jpg')"];
+var img2 = ["url('assets/img/bg2.jpg')","url('assets/img/bg1.jpg')","url('assets/img/bg.jpg')"];
 function prev(){
     document.body.style.background = img2[k];
     k++;
